c1f2f96b78471e17a971560bb4de08101e58e7a2
Devanshi Sharma <121373674+devanshi-1212@users.noreply.github.com>
2024-07-21 19:06:02 +0530

    Update Login.jsx

--- client/src/components/Login/Login.jsx
+++ client/src/components/Login/Login.jsx
@@ -29,7 +29,7 @@ const Login = () => {
     }
 
     try {
-      console.log("im sending req to this http : // 13.235.114.225:5000 / login");      
+      console.log("im sending req to this http : // 13.126.220.129:5000 / login");      
       const url = "http://13.126.220.129:5000/login";
       const response = await fetch(url, {
         method: "POST",
