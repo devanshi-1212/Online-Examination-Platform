20828aa8ed8da821801f9bd69105c11c786265eb
Devanshi Sharma <121373674+devanshi-1212@users.noreply.github.com>
2024-07-21 10:30:39 +0530

    Update AuthValidation.js

--- server/middlewares/AuthValidation.js
+++ server/middlewares/AuthValidation.js
@@ -22,6 +22,8 @@ const loginValidation = (req, res, next) => {
     password: Joi.string().min(4).max(100).required(),
   });
 
+  console.log(req.body);
+
   const { error } = schema.validate(req.body);
 
   if (error) {
